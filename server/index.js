fdb8168b290ea7999a09fafb8558329a232e92c8
satyamprabhat296 <satyamprabhatsingh76@gmail.com>
2026-05-06 22:46:52 +0530

    chore: initial MERN project setup with client and server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+// server/index.js
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
