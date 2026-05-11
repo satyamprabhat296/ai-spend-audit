f8970be8a429b8f440c71cabb929c7e6464b043a
satyamprabhat296 <satyam7015@gmail.com>
2026-05-11 20:57:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://ai-spend-audit-gules.vercel.app/",
+      "https://ai-spend-audit-eta-gray.vercel.app/",
     ],
     methods: ["GET", "POST"],
     credentials: true,
